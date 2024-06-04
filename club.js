3f07fdfcc5516413cc9e0addb9b3c2d01c915d0a
Thierry Billa <thierry.billa96@gmail.com>
2024-06-04 12:04:26 +0200

    add asterix

--- club.js
+++ club.js
@@ -11,7 +11,7 @@ const server = express();
 // Configure CORS
 server.use(cors({
   origin: function (origin, callback) {
-      const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://lokkeroom-frontend-24fab992f120.herokuapp.com/'];
+      const allowedOrigins = ['*'];
       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
           callback(null, true);
       } else {
