bb7ff1516a7c421b2a588f8f08b73260e9de11e0
Thierry Billa <thierry.billa96@gmail.com>
2024-06-04 12:12:52 +0200

    api

--- club.js
+++ club.js
@@ -11,7 +11,7 @@ const server = express();
 // Configure CORS
 server.use(cors({
   origin: function (origin, callback) {
-      const allowedOrigins = ["https://lokkeroom-frontend-24fab992f120.herokuapp.com/"];
+      const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://lokkeroom-frontend-24fab992f120.herokuapp.com/api'];
       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
           callback(null, true);
       } else {
