1272c7d42c205de861c09e504b9d5c83df551d77
Thierry Billa <thierry.billa96@gmail.com>
2024-06-04 12:07:33 +0200

    only local

--- club.js
+++ club.js
@@ -11,7 +11,7 @@ const server = express();
 // Configure CORS
 server.use(cors({
   origin: function (origin, callback) {
-      const allowedOrigins = ['*'];
+      const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
           callback(null, true);
       } else {
